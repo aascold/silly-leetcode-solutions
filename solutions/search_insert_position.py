57b15fca505d71e11f6a71d38b3f432767193798
weedcat671 <dimaming2314@gmail.com>
2025-06-19 11:46:22 +0500

    Search Insert Position

--- solutions/search_insert_position.py
+++ solutions/search_insert_position.py
@@ -6,14 +6,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
-        while r - l > 1:
+
+        while l <= r:
             mid = (l + r) // 2
-            if nums[mid] > target:
-                r = mid
-            else:
-                l = mid
-        if nums[r] < target:
-            return r + 1
-        if nums[l] < target:
-            return l + 1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+
         return l
